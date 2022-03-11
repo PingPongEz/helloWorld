9f44e39e583f601af75a35896e44e4bc75c93008
Sergey <kopgamerk@gmail.com>
2022-03-11 18:16:50 +0300

    Testing commit

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var buttonShowText: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func pressingButton(_ sender: Any) {
+    }
 
 }
 
